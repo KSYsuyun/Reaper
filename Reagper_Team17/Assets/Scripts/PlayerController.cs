333171cf28ceafb488ed55edfc882fc2b6d3f2c6
박은빈 <corwoddl@naver.com>
2022-05-20 18:31:57 +0900

    EunBin
    1. 캐릭터의 기본 좌우 이동
    2. Z를 눌렀을 시 좀 더 빠르게

--- Reagper_Team17/Assets/Scripts/PlayerController.cs
+++ Reagper_Team17/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //PlayerController
+    //5월 20일: EunBin 캐릭터 이동 구현
+
+    Rigidbody2D rigid;
+    SpriteRenderer sr;
+
+    public float movementSpeed = 3.0f;
+
+
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        Move();
+    }
+    
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        // left
+        if (Input.GetAxisRaw("Horizontal") < 0)  //왼쪽
+        {
+            moveVelocity = Vector3.left;
+
+            sr.flipX = true;
+        }
+        // right
+        else if (Input.GetAxisRaw("Horizontal") > 0) //오른쪽
+        {
+            moveVelocity = Vector3.right;
+
+            sr.flipX = false;
+        }
+
+        if(Input.GetKey(KeyCode.Z))
+        {
+            movementSpeed =7;
+        }
+        else
+        {
+            movementSpeed = 3;
+        }
+
+        transform.position += moveVelocity * movementSpeed * Time.deltaTime;
     }
 }
