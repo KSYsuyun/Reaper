5268212db3b047421f9ae2562d9b193819e17e0d
박은빈 <corwoddl@naver.com>
2022-05-20 19:32:40 +0900

    EunBin
    1. 체력 바 수정
    - 달리면 체력 바가 내려가도록
    - 만약 체력이 0까지 내려갔다면, 다시 체력 바가 Full이 될 때까지는 뛰지 못하고 더 느린 속도로 걸어야 함.
    - z축을 누르지 않으면, 체력은 자동으로 채워진다.

--- Reagper_Team17/Assets/Scripts/PlayerController.cs
+++ Reagper_Team17/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,13 @@ public class PlayerController : MonoBehaviour
     //PlayerController
     //5월 20일: EunBin 캐릭터 이동 구현
 
-    public GameObject condiBar; //캐릭터의 체력바를 위한 선언
-
     Rigidbody2D rigid;
     SpriteRenderer sr;
 
     public float movementSpeed = 3.0f;
 
+    public GameObject condiBar; //캐릭터의 체력바를 위한 선언
+    public bool condiZero = false; //컨디션BAr.. 너무 달려서 체력이 0이 됨.
 
     void Start()
     {
@@ -23,8 +23,17 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
         Move();
+        /*        if(!condiZero)//false일때, 즉 체력이 바닥 나지않았을때.. 실행
+                {
+                    //만약 condiZero가 true이면, 움직이지못한다.
+                    Move();
+                }
+                else if (condiZero) //true일때, 즉 체력이 바닥 났을때.. 실행
+                {
+                    //힘들어하는 애니메이션 추가
+                    //!!
+                }*/
     }
     
     void Move()
@@ -46,17 +55,32 @@ public class PlayerController : MonoBehaviour
             sr.flipX = false;
         }
 
-        if (Input.GetKey(KeyCode.Z))
-        {
-            movementSpeed = 7;
-            condiBar.GetComponent<ConditionBar>().currentHP -= 1f;
-        }
-        else
-        {
-            movementSpeed = 3;
 
-            condiBar.GetComponent<ConditionBar>().currentHP += 1f;
+
+        if (!condiZero)//false일때, 즉 체력이 바닥 나지않았을때.. 실행
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                movementSpeed = 7;
+                condiBar.GetComponent<ConditionBar>().currentHP -= 0.5f;
+            }
+            else
+            {
+                movementSpeed = 3;
+
+                condiBar.GetComponent<ConditionBar>().currentHP += 0.2f;
+            }
         }
+        else if (condiZero) //true일때, 즉 체력이 바닥 났을때.. 실행
+        {
+            //힘들어하는 애니메이션 추가
+            movementSpeed = 2;//느려진 스피드..
+
+            condiBar.GetComponent<ConditionBar>().currentHP += 0.2f;
+        }
+
+
+        
         
 
         transform.position += moveVelocity * movementSpeed * Time.deltaTime;
